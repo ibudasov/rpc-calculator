6d321f0d65be79520da9804a7154cb4bd04178df
igorbudasov <igor.budasov@mollie.com>
2020-12-12 11:17:48 +0100

    input element

--- src/main/kotlin/InputElement.kt
+++ src/main/kotlin/InputElement.kt
@@ -1,2 +1,18 @@
+import java.lang.Double.parseDouble
+
 class InputElement(val elementLiteral: String) {
+    fun isInteger(): Boolean {
+
+        try {
+            parseDouble(elementLiteral)
+        } catch (e: NumberFormatException) {
+            return false
+        }
+
+        return true
+    }
+
+    fun asInteger(): Double {
+        return parseDouble(elementLiteral)
+    }
 }
--- src/test/kotlin/InputElementTest.kt
+++ src/test/kotlin/InputElementTest.kt
@@ -1,13 +1,28 @@
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 internal class InputElementTest {
 
     @Test
     fun `inputElement can be constructed from string`() {
-        val ok = InputElement("ok")
+        val elementUnderTheTest = InputElement("ok")
 
-        assertEquals("ok", ok.elementLiteral)
+        assertEquals("ok", elementUnderTheTest.elementLiteral)
+    }
 
+    @Test
+    fun `inputElement can be can be identified as an Int when it's applicable`() {
+        val elementUnderTheTest = InputElement("2")
+
+        assertTrue(elementUnderTheTest.isInteger())
+    }
+
+    @Test
+    fun `inputElement can be can be identified as an NON-Int when it's a word`() {
+        val elementUnderTheTest = InputElement("banaan")
+
+        assertFalse(elementUnderTheTest.isInteger())
     }
 }
